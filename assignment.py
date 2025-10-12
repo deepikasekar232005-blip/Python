4055cafd33fe33cd6dcc4e85471bd2ffa8f6b48d
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-12 06:43:00 +0530

    This is a assignment operator program

---
+++ assignment.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a//=b
+print(a)
+a%=b
+print(a)
+a**b
+print(a)
