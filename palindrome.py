8a83b96df28632926c0bcdee3a8a3e4075918747
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-12 06:47:55 +0530

    This file contain palindrome program

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+num = int(input("Enter an interger:"))
+temp = num
+rev = 0
+while temp != 0:
+    digit = temp % + 10
+    rev = rev * 10 + digit
+    temp //= 10
+if(num == rev):
+    print("True")
+else:
+    print("False")
